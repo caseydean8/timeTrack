483ea6f3e3b8f161abaf94625c53e026d8ae473f
Casey Carroll <caseydean8@gmail.com>
2020-02-20 09:52:34 -0500

    Inputs respond to enter key

--- app.js
+++ app.js
@@ -2,7 +2,20 @@ $("button").on("click", function(event) {
   event.preventDefault();
   const task = $("input").val();
   console.log(task);
+  const taskLabel = $("<label>").text("press to start");
   const taskBtn = $("<button>").addClass("task-button");
   $(taskBtn).text(task);
-  $("#task-list").append(taskBtn);
+  $("#task-list").append(taskLabel, taskBtn);
 })
+
+// function checkSubmit(e) {
+//   if(e && e.keyCode == 13) {
+//     const task = $("input").val();
+//   console.log(task);
+//   const taskLabel = $("<label>").text("press to start");
+//   const taskBtn = $("<button>").addClass("task-button");
+//   $(taskBtn).text(task);
+//   $("#task-list").append(taskLabel, taskBtn);
+//      document.forms[0].submit();
+//   }
+// }
