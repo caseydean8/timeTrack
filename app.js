ba5d4047767670c6a55ccf4cab1bffa4d504233e
Casey Carroll <caseydean8@gmail.com>
2020-02-23 11:27:29 -0500

    Can access label text by name attribute

--- app.js
+++ app.js
@@ -14,28 +14,58 @@ firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 const db = firebase.database();
 
+console.log(moment().format());
+let taskID = 0;
 $("button").on("click", function(event) {
   event.preventDefault();
   const task = $("input").val();
   console.log(task);
-  sendFireBase(task);
+  sendFireBase(task + " added");
   taskButtons(task);
   $("input").val("");
-})
+  taskID++;
+});
 
-const sendFireBase = (task) => {
-  db.ref().push(task);
-}
+const sendFireBase = task => {
+  db.ref()
+    .push(task)
+    .then(err => console.log(err));
+};
 
-const taskButtons = (task) => {
-  const taskLabel = $("<label>").text("press to start");
-  const taskBtn = $("<button>").addClass("task-button");
+const taskButtons = (task, key) => {
+  // const taskForm = $("<form>").attr("id", task);
+  const taskForm = $("<form>").addClass("tasks");
+  const taskLabel = $("<label>")
+    .attr("name", key)
+    .text("press to start");
+  const taskBtn = $("<button>").attr({
+    class: "task-button",
+    id: key,
+    "data-start": false
+  });
   $(taskBtn).text(task);
-  $("#task-list").append(taskLabel, taskBtn);
-}
+  $(taskForm).append(taskLabel, taskBtn);
+  $("#task-list").append(taskForm);
+};
 
 db.ref().on("child_added", function(snapshot) {
   const databaseTask = snapshot.val();
+  const key = snapshot.key;
+  // console.log(snapshot.key);
   console.log(databaseTask);
-  taskButtons(databaseTask);
-})
+  taskButtons(databaseTask, key);
+});
+
+$(document).on("click", ".task-button", function(event) {
+  event.preventDefault();
+  console.log($(this).text() + " clicked");
+  console.log($(this).attr("id"));
+  const name = $(this).attr("id");
+  // console.log($(this).html("<label>"));
+  const startTime = moment().format();
+  console.log(startTime);
+  const labelChange = $(`label[name="${name}"]`);
+  console.log(labelChange);
+  // console.log(labelChange[0].attributes);
+  $(labelChange).text("press to stop");
+});
